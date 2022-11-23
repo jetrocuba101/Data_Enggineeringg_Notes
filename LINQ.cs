b944b424741d2aed8a8d8c3494d42a5a117e23d8
jetrocuba101 <minenclenkosi@gmail.com>
2022-11-23 10:02:20 +0200

    LINQ Notes

---
+++ LINQ.cs
@@ -0,0 +1,14 @@
+# What is LINQ?
+The LINQ (**Language Integrated Query**) is a part of a language but not a complete language. It was introduced by Microsoft with .NET Framework 3.5 and C# 3.0 and is available in System.Linq 
+namespace.
+
+LINQ provides us with common query syntax which allows us to query the data from various data sources. That means using a single query we can get or set the data from various data sources such as 
+SQL Server database, XML documents, ADO.NET Datasets, and any other in-memory objects such as Collections, Generics, etc.
+
+![image linq](https://dotnettutorials.net/wp-content/uploads/2019/04/Linq-applying-on-different-data-sources.png)
+
+## Why use LINQ?
+So, LINQ provides a **uniform programming model which will make it easy to work with different data sources by using a standard coding style. That means we don't require different syntaxes to 
+query different data sources.
+
+![structure](https://dotnettutorials.net/wp-content/uploads/2019/04/Why-should-we-use-linq-in-C.png)
